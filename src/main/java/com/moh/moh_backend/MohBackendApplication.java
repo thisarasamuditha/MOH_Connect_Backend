3684a687f2125cdcfce5fdceaffcc39727b14dc2
thisarasamuditha <thellamburavithanagethisarasam@gmail.com>
2025-11-26 18:41:08 +0530

    my first commit

---
+++ src/main/java/com/moh/moh_backend/MohBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moh.moh_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohBackendApplication.class, args);
+	}
+
+}
