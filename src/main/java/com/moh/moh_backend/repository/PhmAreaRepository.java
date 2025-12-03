faec3689cad77a4617468d2a93fd7c753f60af20
thisarasamuditha <thellamburavithanagethisarasam@gmail.com>
2025-12-03 23:13:59 +0530

    created the PhmAreaRepository

---
+++ src/main/java/com/moh/moh_backend/repository/PhmAreaRepository.java
@@ -0,0 +1,11 @@
+package com.moh.moh_backend.repository;
+
+import com.moh.moh_backend.model.PhmArea;
+import com.moh.moh_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PhmAreaRepository extends JpaRepository<PhmArea, Integer> {
+    Optional<PhmArea> findById(Integer phmAreaId);
+}
