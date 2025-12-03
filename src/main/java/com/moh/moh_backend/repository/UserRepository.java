6824d073426dd074cc3ea02f9def7996fcf7d760
thisarasamuditha <thellamburavithanagethisarasam@gmail.com>
2025-12-03 16:49:55 +0530

    refactor the UserRepository class by removing     "findByUsername" JPA method

--- src/main/java/com/moh/moh_backend/repository/UserRepository.java
+++ src/main/java/com/moh/moh_backend/repository/UserRepository.java
@@ -9,7 +9,6 @@ import java.util.Optional;
  * Repository for USER table operations.
  */
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
