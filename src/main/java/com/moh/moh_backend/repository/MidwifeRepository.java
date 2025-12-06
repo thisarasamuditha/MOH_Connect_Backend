c49eebece602cceb2f3e357bca4deaa4f16627b7
thisarasamuditha <thellamburavithanagethisarasam@gmail.com>
2025-12-06 18:38:56 +0530

    updated the MidwifeRepository

--- src/main/java/com/moh/moh_backend/repository/MidwifeRepository.java
+++ src/main/java/com/moh/moh_backend/repository/MidwifeRepository.java
@@ -3,6 +3,8 @@ package com.moh.moh_backend.repository;
 import com.moh.moh_backend.model.Midwife;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MidwifeRepository extends JpaRepository<Midwife, Integer> {
-    <Optional> Midwife findByMidwifeId(Integer midwife_id);
+    Optional<Midwife> findByUser_UserId(Integer userId);
 }
