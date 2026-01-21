b9d75ae2d7a477bc0cfd5e628600e3851cc7f050
Maheesha <maheeshamihiran2021@gmail.com>
2026-01-21 14:57:04 +0530

    add methods to BabyRepository for querying by mother and pregnancy IDs

--- src/main/java/com/moh/moh_backend/repository/BabyRepository.java
+++ src/main/java/com/moh/moh_backend/repository/BabyRepository.java
@@ -6,3 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+
+public interface BabyRepository extends JpaRepository<Baby, Integer> {
+    List<Baby> findByMotherId(Integer motherId);
+    List<Baby> findByPregnancyId(Integer pregnancyId);
+}
