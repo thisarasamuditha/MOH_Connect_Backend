c117a74cb444ae8749a323cda5585325d8bb79a2
thisarasamuditha <thellamburavithanagethisarasam@gmail.com>
2026-02-04 17:59:24 +0530

    Remove unused import statements from MotherRepository and PhmAreaRepository; clean up AuthService imports

--- src/main/java/com/moh/moh_backend/repository/MotherRepository.java
+++ src/main/java/com/moh/moh_backend/repository/MotherRepository.java
@@ -1,7 +1,6 @@
 package com.moh.moh_backend.repository;
 
 import com.moh.moh_backend.model.Mother;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MotherRepository extends JpaRepository <Mother, Integer> {
--- src/main/java/com/moh/moh_backend/repository/PhmAreaRepository.java
+++ src/main/java/com/moh/moh_backend/repository/PhmAreaRepository.java
@@ -1,7 +1,6 @@
 package com.moh.moh_backend.repository;
 
 import com.moh.moh_backend.model.PhmArea;
-import com.moh.moh_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
